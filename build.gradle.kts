5a04b957f24a37f36c26a83724e7454d24567565
たけのこII <yu2.aoki0229.@gmail.com>
2026-03-18 01:28:09 +0900

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,6 +67,7 @@ tasks {
         inputs.property("minecraft_version", project.property("minecraft_version"))
         inputs.property("loader_version", project.property("loader_version"))
         inputs.property("kotlin_loader_version", project.property("kotlin_loader_version"))
+        inputs.property("fabric_api_version", project.property("fabric_api_version"))
         filteringCharset = "UTF-8"
 
         filesMatching("fabric.mod.json") {
@@ -74,7 +75,8 @@ tasks {
                 "version" to project.version,
                 "minecraft_version" to project.property("minecraft_version")!!,
                 "loader_version" to project.property("loader_version")!!,
-                "kotlin_loader_version" to project.property("kotlin_loader_version")!!
+                "kotlin_loader_version" to project.property("kotlin_loader_version")!!,
+                "fabric_api_version" to project.property("fabric_api_version")!!
             )
         }
     }
